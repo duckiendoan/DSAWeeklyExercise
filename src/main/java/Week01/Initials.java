1169e56fe428b573133a8bb2ab015a65204e9853
duckiendoan <kiendd.hrt@gmail.com>
2022-09-10 23:23:42 +0700

    Added files

---
+++ src/main/java/Week01/Initials.java
@@ -0,0 +1,15 @@
+package Week01;
+
+public class Initials {
+    public static void main(String[] args) {
+        System.out.println("**        ***    **********      **             *             **");
+        System.out.println("**      ***      **        **     **           ***           ** ");
+        System.out.println("**    ***        **         **     **         ** **         **  ");
+        System.out.println("**  ***          **          **     **       **   **       **   ");
+        System.out.println("*****            **          **      **     **     **     **    ");
+        System.out.println("**  ***          **          **       **   **       **   **     ");
+        System.out.println("**    ***        **         **         ** **         ** **      ");
+        System.out.println("**      ***      **        **           ***           ***       ");
+        System.out.println("**        ***    **********              *             *        ");
+    }
+}
