5d3f6ff69dd5504193504bad089e7fb82243beee
duckiendoan <kiendd.hrt@gmail.com>
2022-09-19 21:07:25 +0700

    Done Week05 Ex4

--- src/main/java/Week05/SelectionSort.java
+++ src/main/java/Week05/SelectionSort.java
@@ -1,2 +1,39 @@
-package Week05;public class SelectionSort {
+package Week05;
+
+import edu.princeton.cs.algs4.*;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int N = 100;
+        int[] a = new int[N];
+        for (int i = 0; i < N; i++)
+            a[i] = StdRandom.uniformInt(N);
+        selectionSort(a);
+        for (int i = 0; i < N; i++)
+            StdOut.println(a[i]);
+    }
+
+    public static void selectionSort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            swap(a, i, findMinIndex(a, i));
+        }
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    private static int findMinIndex(int[] a, int start) {
+        int min = a[start];
+        int minIndex = start;
+        for (int i = start + 1; i < a.length; i++) {
+            if (a[i] <= min) {
+                min = a[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
 }
