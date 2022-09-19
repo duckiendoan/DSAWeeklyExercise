d6780ea96442c60c546d4239d57f0a1183fc36fa
duckiendoan <kiendd.hrt@gmail.com>
2022-09-19 21:06:57 +0700

    Done Week05 Ex2

--- src/main/java/Week05/BalancedBrackets.java
+++ src/main/java/Week05/BalancedBrackets.java
@@ -1,2 +1,28 @@
-package Week05;public class BalancedBrackets {
+package Week05;
+import edu.princeton.cs.algs4.*;
+
+// https://www.hackerrank.com/challenges/balanced-brackets
+public class BalancedBrackets {
+    private static char getOpenBracket(char c) {
+        if (c == ')') return '(';
+        if (c == ']') return '[';
+        return '{';
+    }
+    public static String isBalanced(String s) {
+        // Write your code here
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{')
+                stack.push(c);
+            else if (c == ')' || c == ']' || c == '}') {
+                if (stack.isEmpty())
+                    return "NO";
+                char c2 = stack.pop();
+                if (getOpenBracket(c) != c2)
+                    return "NO";
+            }
+        }
+        return stack.isEmpty() ? "YES": "NO";
+    }
 }
