3a5253f6db0f21e78d36c4c3d1da11d48fdb3d37
duckiendoan <kiendd.hrt@gmail.com>
2022-09-27 16:21:17 +0700

    Added sorting algorithm report

--- src/main/java/Week06/SortingAlgorithms.java
+++ src/main/java/Week06/SortingAlgorithms.java
@@ -1,45 +1,107 @@
 package Week06;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 public class SortingAlgorithms {
-    public static <T extends Comparable<T>> void selectionSort(T[] a) {
-        for (int i = 0; i < a.length - 1; i++) {
-            int minIndex = i;
-            for (int j = i; j < a.length; j++)
-                if (a[j].compareTo(a[minIndex]) < 0)
-                    minIndex = j;
-            exch(a, minIndex, i);
+    static class Quick {
+        public static void sort(Comparable[] a) {
+            StdRandom.shuffle(a);
+            sort(a, 0, a.length - 1);
+            assert isSorted(a);
+        }
+
+        private static void sort(Comparable[] a, int lo, int hi) {
+            if (lo >= hi) return;
+            int j = partition(a, lo, hi);
+            sort(a, lo, j - 1);
+            sort(a, j + 1, hi);
+            assert isSorted(a, lo, hi);
+        }
+
+        private static int partition(Comparable[] a, int lo, int hi) {
+            int i = lo, j = hi + 1;
+            while (true)
+            {
+                while (less(a[++i], a[lo]))
+                    if (i == hi) break;
+                while (less(a[lo], a[--j]))
+                    if (j == lo) break;
+
+                if (i >= j) break;
+                exch(a, i, j);
+            }
+            exch(a, lo, j);
+            return j;
         }
     }
-
-    public static <T extends Comparable<T>> void insertionSort(T[] a) {
-        for (int i = 1; i < a.length; i++) {
-            int j = i;
-            while (j > 0 && a[j].compareTo(a[j - 1]) < 0) {
-                exch(a, j, j - 1);
-                j--;
+    static class Merge {
+        public static void sort(Comparable[] a) {
+            Comparable[] aux = new Comparable[a.length];
+            sort(a, aux, 0, a.length - 1);
+        }
+        private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+            if (lo >= hi) return;
+            int mid = lo + (hi - lo) / 2;
+            sort(a, aux, lo, mid);
+            sort(a, aux, mid + 1, hi);
+            merge(a, aux, lo, mid, hi);
+        }
+        private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+            for (int i = lo; i <= hi; i++)
+                aux[i] = a[i];
+            int i = lo, j = mid + 1;
+            for (int k = lo; k <= hi; k++) {
+                if (i > mid) a[k] = aux[j++];
+                else if (j > hi) a[k] = aux[i++];
+                else if (aux[i].compareTo(aux[j]) <= 0) a[k] = aux[i++];
+                else a[k] = aux[j++];
             }
         }
     }
 
-    public static <T extends Comparable<T>> void insertionSortRecursive(T[] a) {
-        insertionSortRecursive(a, a.length - 1);
-    }
-
-    private static <T extends Comparable<T>> void insertionSortRecursive(T[] a, int i) {
-        if (i > 0) {
-            // Sort array a from 0 to i - 1
-            insertionSortRecursive(a, i - 1);
-            // Insert element a[i] to the sorted list
-            insertSorted(a, i);
+    static class Selection {
+        public static <T extends Comparable<T>> void sort(T[] a) {
+            for (int i = 0; i < a.length - 1; i++) {
+                int minIndex = i;
+                for (int j = i; j < a.length; j++)
+                    if (a[j].compareTo(a[minIndex]) < 0)
+                        minIndex = j;
+                exch(a, minIndex, i);
+            }
         }
     }
 
-    private static <T extends Comparable<T>> void insertSorted(T[] a, int i) {
-        if (i > 0 && a[i].compareTo(a[i - 1]) < 0) {
-            // Exchange i and i - 1
-            exch(a, i ,i - 1);
-            // Insert element into array
-            insertSorted(a, i - 1);
+    static class Insertion {
+        public static <T extends Comparable<T>> void sort(T[] a) {
+            for (int i = 1; i < a.length; i++) {
+                int j = i;
+                while (j > 0 && a[j].compareTo(a[j - 1]) < 0) {
+                    exch(a, j, j - 1);
+                    j--;
+                }
+            }
+        }
+
+        public static <T extends Comparable<T>> void sortRecursive(T[] a) {
+            insertionSortRecursive(a, a.length - 1);
+        }
+
+        private static <T extends Comparable<T>> void insertionSortRecursive(T[] a, int i) {
+            if (i > 0) {
+                // Sort array a from 0 to i - 1
+                insertionSortRecursive(a, i - 1);
+                // Insert element a[i] to the sorted list
+                insertSorted(a, i);
+            }
+        }
+
+        private static <T extends Comparable<T>> void insertSorted(T[] a, int i) {
+            if (i > 0 && a[i].compareTo(a[i - 1]) < 0) {
+                // Exchange i and i - 1
+                exch(a, i ,i - 1);
+                // Insert element into array
+                insertSorted(a, i - 1);
+            }
         }
     }
 
@@ -50,10 +112,21 @@ public class SortingAlgorithms {
         return true;
     }
 
+    public static <T extends Comparable<T>> boolean isSorted(T[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (a[i].compareTo(a[i-1]) < 0)
+                return false;
+        return true;
+    }
+
     private static <T extends Comparable<T>> void exch(T[] a, int i, int j)
     {
         T swap = a[i];
         a[i] = a[j];
         a[j] = swap;
+    }
+
+    private static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
     }
 }
--- src/main/java/Week06/SortingTest.java
+++ src/main/java/Week06/SortingTest.java
@@ -2,7 +2,19 @@ package Week06;
 
 import edu.princeton.cs.algs4.*;
 
+import java.util.Arrays;
+
 public class SortingTest {
+    // Colors from https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
     private static final String leftAlignFormat = "| %-15s | %-11s | %-11s | %-11s | %-19s | %-12s |%n";
     interface SortingAlgo {
         void sort(Comparable[] a);
@@ -13,27 +25,26 @@ public class SortingTest {
     }
 
     public static void main(String[] args) {
-        System.out.format("+-----------------+-------------+-------------+-------------+---------------------+--------------+%n");
-        System.out.format("| Algorithm       | Test data   | Random data | Sorted data | Reverse-sorted data | Uniform data |%n");
-        System.out.format("+-----------------+-------------+-------------+-------------+---------------------+--------------+%n");
+        System.out.format(ANSI_BLUE + "+-----------------+-------------+-------------+-------------+---------------------+--------------+%n");
+        System.out.format("| Algorithm       | algs4-data  | Random data | Sorted data | Reverse-sorted data | Uniform data |%n");
+        System.out.format("+-----------------+-------------+-------------+-------------+---------------------+--------------+%n" + ANSI_RESET);
         addReportFor(Selection::sort, "SelectionSort");
         addReportFor(Insertion::sort, "InsertionSort");
         addReportFor(Merge::sort, "MergeSort");
         addReportFor(Quick::sort, "QuickSort");
-        addReportFor(Shell::sort, "ShellSort");
     }
 
     private static void addReportFor(SortingAlgo algo, String algoName) {
         long time1 = measureSortingAlgorithm(algo,
-                TestDataGenerator::KInts, 32, 3);
+                TestDataGenerator::KInts, 16, 3);
         long time2 = measureSortingAlgorithm(algo,
-                TestDataGenerator::randomData, 32000, 5);
+                TestDataGenerator::randomData, 16000, 5);
         long time3 = measureSortingAlgorithm(algo,
-                TestDataGenerator::sortedData, 32000, 3);
+                TestDataGenerator::sortedData, 16000, 3);
         long time4 = measureSortingAlgorithm(algo,
-                TestDataGenerator::reverseSortedData, 32000, 3);
+                TestDataGenerator::reverseSortedData, 16000, 3);
         long time5 = measureSortingAlgorithm(algo,
-                TestDataGenerator::uniformData,32000, 1);
+                TestDataGenerator::uniformData,16000, 1);
         System.out.format(leftAlignFormat, algoName, time1 + "ms", time2 + "ms", time3 + "ms", time4 + "ms", time5 + "ms");
         System.out.format("+-----------------+-------------+-------------+-------------+---------------------+--------------+%n");
     }
